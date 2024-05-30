70dfe5e9b14a392b432fc9433922bf417fb47593
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-05-30 16:08:30 +0300

    feat: ship object created

--- src/utility.js
+++ src/utility.js
@@ -1,4 +1,30 @@
+/**
+ *ship  has name,length and hits,position as property and isSunk and hit as method
+ *ship.position - is an array that hold coordinates of the ship.
+ *ship.isSunk() - to check the ship is sunk or not return boolean
+ *ship.hit(ship.hits) - increase ship hits on by one each time it is called.
+ * @param {*} shipName
+ * @param {*} length
+ */
+function Ship(shipName, length, hits = 0) {
+  function hit() {
+    this.hits++;
+  }
+  function isSunk() {
+    return length <= this.hits ? true : false;
+  }
+
+  return {
+    shipName,
+    length,
+    hits,
+    positions: [],
+    hit,
+    isSunk,
+  };
+}
 function sum(a, b) {
   return a + b;
 }
-export { sum };
+
+export { sum, Ship };
