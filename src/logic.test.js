fdd2553f21256c837ac12c63ab7dfe9274814c97
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-05-30 16:26:16 +0300

    test: added to check ship position

--- src/logic.test.js
+++ src/logic.test.js
@@ -1,17 +1,29 @@
 /* eslint-disable no-undef */
-import { sum } from "./utility";
+import { sum, Ship } from "./utility";
 /*eslint no-undef: "error"*/
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-const submarine = Ship("submarine", 3);
+const submarine = Ship("submarine", 3, 0);
+submarine.hit();
+submarine.hit();
+submarine.hit();
 describe("submarine", () => {
-  test("is length"),
-    () => {
-      expect(submarine.length).toBeEqual(3);
-    };
+  test("is length", () => {
+    expect(submarine.length).toEqual(3);
+  });
+  test("number of hits", () => {
+    expect(submarine.hits).toEqual(3);
+  });
   test("is sunk", () => {
-    expect(submarine.isSunk()).toBeFalsy();
+    expect(submarine.isSunk()).toBeTruthy();
+  });
+});
+describe("board", () => {
+  test("is ship position vertical", () => {
+    expect(submarine.positions).toContain("0,0");
+    expect(submarine.positions).toContain("0,1");
+    expect(submarine.positions).toContain("0,2");
   });
 });
