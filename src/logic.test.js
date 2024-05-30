5d9f290babf8afc8f23f6e8b817cf06112c603a8
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-05-30 09:41:20 +0300

    test: for ship factory function created

--- src/logic.test.js
+++ src/logic.test.js
@@ -4,3 +4,14 @@ import { sum } from "./utility";
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+const submarine = Ship("submarine", 3);
+describe("submarine", () => {
+  test("is length"),
+    () => {
+      expect(submarine.length).toBeEqual(3);
+    };
+  test("is sunk", () => {
+    expect(submarine.isSunk()).toBeFalsy();
+  });
+});
