f3eb600e7d366efcf52cb291c1b0068e2ace9b90
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-06-24 04:55:36 +0300

    feat: function created to show sunk ship  for each player

--- src/dom-stuff.js
+++ src/dom-stuff.js
@@ -1,28 +1,22 @@
-import { Ship, Player } from "./utility";
+import { Player } from "./utility";
 import { strikeBoard, firstBoard } from "./bn";
 import { introPage } from "./first-page/first-page.js";
+import { dragShips } from "./place-ship-page/ship-position.js";
 
-const carrier = Ship("carrier", 5);
-const battleShip = Ship("battleShip", 4);
-const destroyer = Ship("destroyer", 3);
-const submarine = Ship("submarine", 3);
-const patrol = Ship("patrol", 2);
 //
-const ships = [carrier, submarine, battleShip, destroyer, patrol];
-const playerOne = Player("kings", ships);
-const playerTwo = Player("cold", ships);
+//const ships = [carrier, submarine, battleShip, destroyer, patrol];
+//let playerOne = Player("kings", ships);
+//let playerTwo = Player("cold", ships);
 //function that place ships randomly
+let count = 3;
 function initialBoard(player) {
-  ships.forEach((ship) => {
+  player.board.shipsArray.forEach((ship) => {
     player.board.placeRandom(ship);
   });
+  console.log(player);
   return player;
 }
 
-//countdown 3sec to pass device
-function passDevice() {
-  setTimeout(() => {}, timeout);
-}
 //const placePlayerOneSHip = initialBoard(playerOne);
 //const placePlayerTwoShip = initialBoard(playerTwo);
 /*
@@ -32,9 +26,7 @@ function passDevice() {
  return 10 x 10 board one with ship shown the other without the ship to show the opponent striking state on the board. 
 */
 
-function GameFlow() {
-  const placePlayerOneSHip = initialBoard(playerOne);
-  const placePlayerTwoShip = initialBoard(playerTwo);
+function GameFlow(playerOne, playerTwo) {
   console.log(playerOne);
   console.log(playerTwo);
   const players = [playerOne, playerTwo];
@@ -51,32 +43,44 @@ function GameFlow() {
     const allCoordinateHashmap = player.board.coordinatesHashmap;
     const shipBoardState = firstBoard(allTheShips, hitStrikes, missStrikes);
     const strikeBoardState = strikeBoard(allTheShips, hitStrikes, missStrikes);
+    const updateSunkShip = player.board.sunkShips();
     return {
       shipBoardState,
       strikeBoardState,
+      updateSunkShip,
     };
   };
   const printNewBoard = () => {
     //draw current player board state using opponent hit and miss
     //then draw striking board using current player hit and miss on opponent board
+    //changeTurn();
+
     changeTurn();
     const shipBoard = printBoard(getPlayer()).shipBoardState;
+    const secondPlayerShipState = printBoard(getPlayer()).updateSunkShip;
+    const playerTwoName = getPlayer().name;
     changeTurn();
+    const playerOneName = getPlayer().name;
+    const firstPlayerShipState = printBoard(getPlayer()).updateSunkShip;
     const strikeBoard = printBoard(getPlayer()).strikeBoardState;
 
     return {
+      playerOneName,
+      playerTwoName,
       shipBoard,
       strikeBoard,
+      firstPlayerShipState,
+      secondPlayerShipState,
     };
   };
   const playerRound = (player, clickedNum) => {
     const coordinate = player.board.coordinatesHashMap.get(Number(clickedNum));
+    console.log(player);
     player.board.receiveAttack(coordinate);
     printNewBoard();
     changeTurn();
   };
 
-  printNewBoard();
   return {
     getPlayer,
     playerRound,
@@ -86,20 +90,48 @@ function GameFlow() {
 /**
  * function that update the screen using game flow function
  */
-function screenController() {
-  const game = GameFlow();
+function screenController(playerOne, playerTwo) {
+  const game = GameFlow(playerOne, playerTwo);
   const turn = document.querySelector(".player-turn");
   const playerOneShipsBoard = document.querySelector(".board-one");
   const playerOneStrikeBoard = document.querySelector(".board-two");
-  const playerTwoShipsBoard = document.querySelector(".board-three");
-  const playerTwoStrikeBoard = document.querySelector(".board-four");
+  const firstPlayerShips = document.querySelector(".player-one-mini-ships");
+  const secondPlayerShips = document.querySelector(".player-two-mini-ships");
+  firstPlayerShips.textContent = "";
+  secondPlayerShips.textContent = "";
+  drawMiniShips(firstPlayerShips, playerOne.name);
+  drawMiniShips(secondPlayerShips, playerTwo.name);
+
   const updateScreen = () => {
     playerOneShipsBoard.textContent = "";
     playerOneStrikeBoard.textContent = "";
-    playerTwoShipsBoard.textContent = "";
-    playerTwoStrikeBoard.textContent = "";
-    //render
     turn.textContent = `${game.getPlayer().name}`;
+    const playerOneName = game.printNewBoard().playerOneName;
+    const playerTwoName = game.printNewBoard().playerTwoName;
+
+    const playerOneDashBoard = document.querySelector(`.${playerOneName}`);
+    const PlayerOneMiniShips =
+      playerOneDashBoard.querySelectorAll(".mini-ship-size");
+    const playerTwoDashBoard = document.querySelector(`.${playerTwoName}`);
+    const playerTwoMiniShips =
+      playerTwoDashBoard.querySelectorAll("mini-ship-size");
+    console.log(game.printNewBoard().firstPlayerShipState);
+    console.log(game.printNewBoard().secondPlayerShipState);
+
+    updateMiniShips(
+      PlayerOneMiniShips,
+      game.printNewBoard().secondPlayerShipState,
+      "red"
+    );
+
+    updateMiniShips(
+      playerTwoMiniShips,
+      game.printNewBoard().firstPlayerShipState,
+      "red"
+    );
+
+    console.log(game.printNewBoard().secondPlayerShipState);
+    console.log(game.printNewBoard().firstPlayerShipState);
 
     playerOneShipsBoard.appendChild(game.printNewBoard().shipBoard);
     // playerTwoShipsBoard.appendChild(game.printNewBoard().shipBoard);
@@ -111,14 +143,10 @@ function screenController() {
     game.playerRound(player, e);
     updateScreen();
   }
+
   playerOneStrikeBoard.addEventListener("click", (e) => {
     const index = e.target.dataset.index;
-    console.log(e.target);
-    clickHandler(index);
-  });
-  playerTwoStrikeBoard.addEventListener("click", (e) => {
-    const index = e.target.dataset.index;
-    console.log(e.target);
+    console.log(index);
     clickHandler(index);
   });
   //initial render
@@ -126,8 +154,8 @@ function screenController() {
 }
 //screenController();
 function drawFirstPage() {
-  const firstPageDiv = document.querySelector("[data-first-page]");
-  firstPageDiv.appendChild(introPage());
+  const pageContainer = document.querySelector("[data-page-container]");
+  pageContainer.appendChild(introPage());
   const logoDiv = document.querySelector(".logo-holder");
   const tittle = document.querySelector("h1");
   //setTimeout(() => {
@@ -137,9 +165,16 @@ function drawFirstPage() {
 }
 //draw ship placment page
 function templateShipGrid(element) {
+  const secondPage = document.createElement("div");
+
+  const strategyBoard = document.createElement("div");
+  strategyBoard.classList.add("board-container");
+  strategyBoard.appendChild(drawGrids());
+  const btns = document.createElement("div");
+
+  //strategyBoard.classList.add("container-holder");
   const template = `
-  <div class="board-container"></div>
-  <div class="ships-container" data-ships-container>
+   <div class="ships-container" data-ships-container>
     <div class="all-ships"></div>
   </div>
   <div class="place-ships-btns">
@@ -158,12 +193,17 @@ function templateShipGrid(element) {
       Randomize
     </button>
     <button class="play-btn">Play</button>
+   
   </div>
-  <button class="play-btn">Play</button>
-  `;
-  element.innerHTML = template;
-  //return divHolder;
+ `;
+  btns.innerHTML = template;
+
+  secondPage.appendChild(strategyBoard);
+  secondPage.appendChild(btns);
+  return secondPage;
 }
+//
+
 function drawGrids() {
   const grid = document.createElement("div");
   grid.classList.add("board");
@@ -176,54 +216,22 @@ function drawGrids() {
   return grid;
 }
 function shipsPlacement(element) {
-  templateShipGrid(element);
+  element.textContent = "";
+  element.appendChild(templateShipGrid());
+}
+function secondPlayerShipPlacement(element) {
+  element.textContent = "";
+  element.appendChild(secondPlayerTemplate());
 }
 function drawBoard(element) {
-  element.textContent = "";
   element.appendChild(drawGrids());
-}
-
-function randomizeShips(newPlayer) {
-  const container = document.querySelector(".board-container");
-  const randomizeBtn = document.querySelector("[data-random-btn]");
-  const shipsContainer = document.querySelector("[data-ships-container]");
-  //const playBtn = document.querySelector(".play-btn");
-  //playBtn.style.display = "none";
-
-  randomizeBtn.addEventListener("click", () => {
-    if (newPlayer.board.shipsPositions.length === 0) {
-      const setShipsPlace = initialBoard(newPlayer);
-      const ships = newPlayer.board.shipsPositions;
-      const hits = newPlayer.board.hitShots;
-      const missed = newPlayer.board.missedShots;
-      container.textContent = "";
-      container.appendChild(firstBoard(ships, hits, missed));
-      console.log(newPlayer);
-
-      playBtn.style.display = "block";
-      playBtn.addEventListener("click", () => {});
-    } else {
-      const keepName = newPlayer.name;
-      newPlayer = null;
-      newPlayer = Player(keepName, ships);
-      const setShipsPlace = initialBoard(newPlayer);
-      const shipsCoordinates = newPlayer.board.shipsPositions;
-      const hits = newPlayer.board.hitShots;
-      const missed = newPlayer.board.missedShots;
-      container.textContent = "";
-      container.appendChild(firstBoard(shipsCoordinates, hits, missed));
-      console.log(newPlayer);
-
-      playBtn.style.display = "flex";
-      playBtn.addEventListener("click", () => {});
-    }
-  });
 }
 
 function randomPlacement(newPlayer) {
   const container = document.querySelector(".board-container");
-
-  //const shipsContainer = document.querySelector("[data-ships-container]");
+  const playBtn = document.querySelector(".play-btn");
+  const shipsContainer = document.querySelector("[data-ships-container]");
+  shipsContainer.style.display = "none";
   if (newPlayer.board.shipsPositions.length === 0) {
     const setShipsPlace = initialBoard(newPlayer);
     const ships = newPlayer.board.shipsPositions;
@@ -231,9 +239,7 @@ function randomPlacement(newPlayer) {
     const missed = newPlayer.board.missedShots;
     container.textContent = "";
     container.appendChild(firstBoard(ships, hits, missed));
-    console.log(newPlayer);
-    return newPlayer;
-    //playBtn.style.display = "block";
+    playBtn.style.display = "block";
   } else {
     const keepName = newPlayer.name;
     newPlayer = null;
@@ -244,12 +250,14 @@ function randomPlacement(newPlayer) {
     const missed = newPlayer.board.missedShots;
     container.textContent = "";
     container.appendChild(firstBoard(shipsCoordinates, hits, missed));
-    console.log(newPlayer);
-    return newPlayer;
-    //playBtn.style.display = "flex";
+    playBtn.style.display = "block";
   }
 }
-function countDown(element) {
+//countdown page
+function countDownPage() {
+  const passScreen = document.querySelector(".pass-screen");
+  const dialogHolder = document.createElement("div");
+  dialogHolder.classList.add("modal-holder");
   const modal = ` <dialog data-countdown>
       <div class="holder">
         <div class="msg-text" data-msg>pass the device</div>
@@ -258,21 +266,57 @@ function countDown(element) {
         </div>
       </div>
     </dialog>`;
-  element.innerHTML = modal;
+  dialogHolder.innerHTML = modal;
+  passScreen.appendChild(dialogHolder);
+}
+function countdownModal() {
+  countDownPage();
+  const dialog = document.querySelector("[data-countdown]");
+  countdown(dialog);
+}
+function updateCountdownUI() {
+  document.querySelector("[data-count-down]").textContent = count;
+  const dialog = document.querySelector("[data-countdown]");
+  if (count === 0) {
+    dialog.close();
+  } else {
+    dialog.showModal();
+  }
 }
 
-function counter(dialog) {
-  let count = 4;
-  const countdown = (document.querySelector("[data-count-down]").textContent =
-    count);
-  count = count - 1;
-  dialog.showModal();
+function countdown(dialog) {
   if (count >= 0) {
-    setTimeout(counter, 1000);
-  } else {
-    clearTimeout(counter);
-    dialog.close();
+    updateCountdownUI(dialog);
+    count--;
+    setTimeout(countdown, 1000);
   }
+}
+//function to daraw mini ships to show ship  sunk state by creating class name using player name to update
+function drawMiniShips(ele, player) {
+  const miniFleets = `
+  <div class="fleet-holder ${player}">
+    <div>${player}</div>
+<div class="mini-carrier  mini-ship-size" data-name='carrier'></div>
+<div class="mini-battleShip  mini-ship-size" data-name='battleShip'></div>
+<div class="same-size-ships">
+  <div class="mini-destroyer  mini-ship-size" data-name='destroyer'></div>
+  <div class="mini-submarine mini-ship-size" data-name='submarine'></div>
+</div>
+<div class="mini-patrol mini-ship-size" data-name='patrol'></div>
+</div>`;
+  ele.innerHTML = miniFleets;
+}
+//function accept mini ships divs, sunk ship names as an array and change color of divs using dataset same as ship name
+
+function updateMiniShips(shipsDiv, sunkShipArray, color) {
+  if (sunkShipArray.length === 0) return;
+  shipsDiv.forEach((ship) => {
+    sunkShipArray.forEach((sunkShip) => {
+      if (ship.dataset.name === sunkShip) {
+        ship.style.backgroundColor = color;
+      }
+    });
+  });
 }
 export {
   screenController,
@@ -281,10 +325,8 @@ export {
   templateShipGrid,
   shipsPlacement,
   drawGrids,
-  randomizeShips,
-  countDown,
-  ships,
-  counter,
+  countdownModal,
   randomPlacement,
   drawBoard,
+  dragShips,
 };
